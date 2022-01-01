e673e86c323b25f0b01d558a4cb4da321bdb16ca
zzinimon <jeeheekim94@naver.com>
2022-01-01 16:05:18 +0900

    Update gitignore

--- src/myProject/OrganizeMyComputer.java
+++ src/myProject/OrganizeMyComputer.java
@@ -5,17 +5,7 @@ import java.io.File;
 public class OrganizeMyComputer {
 
 	public static void main(String[] args) {
-		String path="";
-//		String test="C:\\";
-		File[] files=ShowFolderOnly(path);
-		for (int i = 0; i < files.length; i++) {
-			String str=files[i].toString();
-			if(str.contains("WinSAT")) continue;
-			if(folderIsNull(str)) {
-				System.out.println(files[i]);
-				//files[i].delete();
-			}
-		}
+		//MainOnly.java
 	}
  //해당 폴더의 모든 파일 보기
 	static File[] dir(String path) {
